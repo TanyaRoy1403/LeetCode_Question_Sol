c1931920324be0dfbc9acd6c7570a57f91300817
Tana Roy <roytanya1403@gmail.com>
2024-08-06 15:53:06 +0530

    added question

--- Array2/Range_sum_1508.cpp
+++ Array2/Range_sum_1508.cpp
@@ -2,6 +2,13 @@
 #include<string>
 #include<vector>
 #include<algorithm>
+/*You are given the array nums consisting of n positive integers. You computed the sum of all non-empty continuous subarrays from the array and 
+then sorted them in non-decreasing order, creating a new array of n * (n + 1) / 2 numbers.
+
+Return the sum of the numbers from index left to index right (indexed from 1), inclusive, in the new array. 
+Since the answer can be a huge number return it modulo 109 + 7.Input: nums = [1,2,3,4], n = 4, left = 1, right = 5
+Output: 13*/
+
 using namespace std;
 const int MOD =1e9+7;
 
--- Array2/product_array_except_self.cpp
+++ Array2/product_array_except_self.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+/*Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.*/
+
 vector<int> solve_1(vector<int>&arr){
     //suffix and prefix product alg alg
     int n=arr.size();
