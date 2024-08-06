117585c8012303734f30215e8d048db5ed5dd998
Tana Roy <roytanya1403@gmail.com>
2024-08-06 17:19:18 +0530

    LeetSolution

--- LinkedList/Palindrome_ll.cpp
+++ LinkedList/Palindrome_ll.cpp
@@ -26,8 +26,9 @@ Node* insert_end(int value,Node*& head){
         }
     return head;
 }
+/*//*----------------------Brute force----------------------------------------------------------------------------*/
 bool pali_LL(Node* &head){
-    vector<int>arr;
+    vector<int>arr; //*extra space
     Node* temp=head;
     while(temp!=NULL){
     arr.push_back(temp->data);
@@ -44,6 +45,45 @@ bool pali_LL(Node* &head){
     }
     return true;
 }
+
+/*//*------------------------------------------optimal sol---------------------------------------------------*/
+//rerverse first half while finding mid and then compare
+//agr odd number of nodes h -->fast null nhi hoga yeh last node pe jaa ke ruk jayega ,tb slow to ek aage move then compare with prev
+
+bool palindrome_ll(Node* &head){
+    //empty
+   
+    if(head!=NULL||head->next!=NULL){
+        return true;
+    }
+        Node* prev=NULL;
+        Node* slow=head;
+        Node* fast=head;
+        while(fast!=NULL&&fast->next!=NULL){
+            Node* slowkaagla=slow->next;
+            slow->next=prev; //reverse
+            slow=slow->next;
+            prev=slow;
+            fast=fast->next->next;
+        }
+        //if odd number of nodes
+        if(fast!=NULL){
+            slow=slow->next;
+        }
+        //compare
+        while(prev!=NULL&&slow!=NULL){
+            if(prev->data!=slow->data){
+                return fast;
+            }
+            prev=prev->next;
+            slow=slow->next;
+
+        }
+    return true;
+}
+/*---Time complexity:O(n)---space complexity:O(1)------*/
+
+
 int main(){
     Node* head=NULL;
     insert_end(1,head);
@@ -51,7 +91,7 @@ int main(){
     insert_end(3,head);
     insert_end(2,head);
     insert_end(1,head);
-    bool result=pali_LL(head);
+    bool result=palindrome_ll(head);
     if(result){
         cout<<"it is pali"<<endl;
     }else{
